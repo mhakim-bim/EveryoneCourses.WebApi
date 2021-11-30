ea36f9fb7333de3f9b76e6dd43318ff04c93f2e8
Mohamed Elhakim <melhakim2016@gmail.com>
2021-11-30 23:50:35 +0200

    added authentication identity core

---
+++ EveryoneCourses.ClassLibrary/Migrations/20211129074235_added courses to teachers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EveryoneCourses.ClassLibrary.Migrations
+{
+    public partial class addedcoursestoteachers : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2021, 11, 29, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2021, 11, 29, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 1,
+                column: "CreatedDate",
+                value: new DateTime(2021, 11, 25, 0, 0, 0, 0, DateTimeKind.Local));
+
+            migrationBuilder.UpdateData(
+                table: "Courses",
+                keyColumn: "CourseId",
+                keyValue: 2,
+                column: "CreatedDate",
+                value: new DateTime(2021, 11, 25, 0, 0, 0, 0, DateTimeKind.Local));
+        }
+    }
+}
